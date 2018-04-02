a8c525a405f63aa306e585bf31f3270353a6ea66
camwhaler <camwhaler@gmail.com>
2018-04-02 11:21:25 -0500

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,13 +19,6 @@
 		var	$window = $(window),
 			$body = $('body');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
-
 		// CSS polyfills (IE<9).
 			if (skel.vars.IEVersion < 9)
 				$(':last-child').addClass('last-child');
@@ -33,13 +26,6 @@
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
 
 		// Scrolly links.
 			$('.scrolly').scrolly();
